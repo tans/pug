cd7334c92bb232146dfc91aec5dee1c8e197c6d6
fun <jun@mankei.cc>
2024-04-08 08:37:50 +0800

    -

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const Wechaty = require("wechaty");
-const { PuppetXp } = require("wechaty-puppet-xp");
+let bot = require("./lib/bot");
 
-const puppet = new PuppetXp();
-const { WechatyBuilder } = Wechaty;
-
-const bot = WechatyBuilder.build({
-    puppet: puppet,
-    name: "bot",
-});
-
-bot.on("message", async (msg) => {
-    let text = msg.text();
-    if (text == "ping") {
-        await msg.say("pong");
-    }
-});
-
-bot.start().catch(console.error);
+bot.start();
